46e1df9264ce7efc2d2cbb9e8af2e8b8eceb9b09
Weiwei Yang <ona.ona@gmail.com>
2023-10-25 02:02:32 +0000

    add bigbench and gsm to core scenarios, add script to install helm locally

---
+++ install-helm-local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+python3 -m venv private_helm_env
+source private_helm_env/bin/activate
+
+pip install --upgrade pip
+pip install -e .
+pip install summ-eval
+pip install bert-score
+pip install numba
+pip install 'jieba==0.42.1'
+
