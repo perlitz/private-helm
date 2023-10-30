df4cbecc9f93c318307784344b8633b259125471
Greg Bowyer <gbowyer@fastmail.co.uk>
2023-10-30 14:41:50 -0700

    Add shell script to somewhat automate runs

---
+++ do-run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+source /helm/private_helm_env/bin/activate
+
+export CONF_NAME=$(basename -s .conf "$1")
+
+timeout --foreground 210m helm-run \
+  --output-path /results \
+  --conf-paths "$1" \
+  --suite "$2" \
+  --max-eval-instances 100 \
+  -n 30 | tee "/results/helm-run-$CONF_NAME-$2.log"
+
+helm-summarize \
+  --output-path /results \
+  --suite "$2" \
+  -n 30 | tee "/results/helm-summarize-$CONF_NAME-$2.log"
